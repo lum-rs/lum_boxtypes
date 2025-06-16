8e9a0a9019cb9a0e14f869db0f2bed693e91788f
Torben Schweren <torben@schweren.dev>
2025-06-16 22:28:02 +0200

    add: BoxedErrorResult<T>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 use std::{error::Error, future::Future, pin::Pin};
 
 pub type BoxedError = Box<dyn Error + Send + Sync>;
+pub type BoxedErrorResult<T> = Result<T, BoxedError>;
 
 pub type PinnedBoxedFuture<T> = Pin<Box<dyn Future<Output = T> + Send + Sync>>;
-pub type PinnedBoxedFutureResult<T> = PinnedBoxedFuture<Result<T, BoxedError>>;
+pub type PinnedBoxedFutureResult<T> = PinnedBoxedFuture<BoxedErrorResult<T>>;
 
 pub type LifetimedPinnedBoxedFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + Sync + 'a>>;
 pub type LifetimedPinnedBoxedFutureResult<'a, T> =
-    LifetimedPinnedBoxedFuture<'a, Result<T, BoxedError>>;
+    LifetimedPinnedBoxedFuture<'a, BoxedErrorResult<T>>;
