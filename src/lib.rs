bf3830da2ceb7199632cb80402522a992516f1b5
Torben Schweren <torben@schweren.dev>
2025-12-30 02:26:28 +0100

    add: explicit sync types

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,17 @@ use std::{error::Error, future::Future, pin::Pin};
 pub type BoxedError = Box<dyn Error + Send + Sync>;
 pub type BoxedErrorResult<T> = Result<T, BoxedError>;
 
-pub type PinnedBoxedFuture<T> = Pin<Box<dyn Future<Output = T> + Send + Sync>>;
+pub type PinnedBoxedFuture<T> = Pin<Box<dyn Future<Output = T> + Send>>;
 pub type PinnedBoxedFutureResult<T> = PinnedBoxedFuture<BoxedErrorResult<T>>;
 
-pub type LifetimedPinnedBoxedFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + Sync + 'a>>;
+pub type PinnedBoxedSyncFuture<T> = Pin<Box<dyn Future<Output = T> + Send + Sync>>;
+pub type PinnedBoxedSyncFutureResult<T> = PinnedBoxedSyncFuture<BoxedErrorResult<T>>;
+
+pub type LifetimedPinnedBoxedFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + 'a>>;
 pub type LifetimedPinnedBoxedFutureResult<'a, T> =
     LifetimedPinnedBoxedFuture<'a, BoxedErrorResult<T>>;
+
+pub type LifetimedPinnedBoxedSyncFuture<'a, T> =
+    Pin<Box<dyn Future<Output = T> + Send + Sync + 'a>>;
+pub type LifetimedPinnedBoxedSyncFutureResult<'a, T> =
+    LifetimedPinnedBoxedSyncFuture<'a, BoxedErrorResult<T>>;
